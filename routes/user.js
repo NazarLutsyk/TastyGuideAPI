181be5ff7747392d1509810a133a1c6f8bd0ce3a
NazarLutsyk <nlutsik1@gmail.com>
2018-05-31 18:14:08 +0300

    change principal route

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,38 @@
-let router = require('express').Router();
-let passport = require('passport');
-let PassportMiddleware = require('../middleware/passport');
-let UserController = require('../controllers/UserController');
+let router = require("express").Router();
+let passport = require("passport");
+let PassportMiddleware = require("../middleware/passport");
+let UserController = require("../controllers/UserController");
+let query = require("../middleware/query");
+let Client = require('../models/Client');
 
-router.post('/local/signup', PassportMiddleware.notLoggedIn, UserController.signUp);
-router.post('/local/signin', PassportMiddleware.notLoggedIn, UserController.signIn);
-router.get('/logout', PassportMiddleware.isLoggedIn, UserController.logout);
+router.post("/local/signup", PassportMiddleware.notLoggedIn, UserController.signUp);
+router.post("/local/signin", PassportMiddleware.notLoggedIn, UserController.signIn);
+router.get("/logout", PassportMiddleware.isLoggedIn, UserController.logout);
 
-router.get('/facebook', passport.authenticate('facebook'));
-router.get('/facebook/callback',
+router.get("/facebook", passport.authenticate("facebook"));
+router.get("/facebook/callback",
     PassportMiddleware.notLoggedIn,
-    passport.authenticate('facebook'),
+    passport.authenticate("facebook"),
     UserController.facebookAuth);
 
-router.get('/google', passport.authenticate('google', {
+router.get("/google", passport.authenticate("google", {
     scope:
-        ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read']
+        ["https://www.googleapis.com/auth/plus.login", "https://www.googleapis.com/auth/plus.profile.emails.read"]
 }));
-router.get('/google/callback',
+router.get("/google/callback",
     PassportMiddleware.notLoggedIn,
-    passport.authenticate('google'),
+    passport.authenticate("google"),
     UserController.googleAuth);
 
 
-router.get('/principal', function (req, res) {
-    res.json(req.user);
+router.get("/principal", query.parseQuery, async function (req, res) {
+    if (req.user) {
+        req.query.query._id = req.user._id;
+        let client = await Client.superfind(req.query);
+        res.json(client[0]);
+    } else {
+        res.json(null);
+    }
 });
 
 module.exports = router;
