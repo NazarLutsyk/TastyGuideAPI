8e077431258447374bfa734d5db02fcde4dcaffe
NazarLutsyk <nlutsik1@gmail.com>
2018-06-11 16:45:47 +0300

    change file upload in place

--- controllers/PlaceController.js
+++ controllers/PlaceController.js
@@ -1,4 +1,5 @@
 let path = require("path");
+let fileHelper = require('../helpers/fileHelper');
 let Place = require("../models/Place");
 let Review = require("../models/Review");
 let Department = require("../models/Department");
@@ -58,38 +59,18 @@ module.exports = {
             } else {
                 let place = new Place(req.body);
                 if (place) {
-                    upload(req, res, async function (err) {
-                        if (err) {
-                            return res.status(400).send(err.toString());
-                        } else {
-                            if (!place.images)
-                                place.images = [];
-                            for (let field in req.files) {
-                                for (let fileKey in req.files[field]) {
-                                    if (req.files[field][fileKey].fieldname === "avatar") {
-                                        let image = "/upload/place/" + req.files[field][fileKey].filename;
-                                        place.avatar = image;
-                                    }
-                                    if (req.files[field][fileKey].fieldname === "images") {
-                                        let image = "/upload/place/" + req.files[field][fileKey].filename;
-                                        place.images.push(image);
-                                    }
-                                }
-                            }
-                            try {
-                                place = await place.supersave();
-                                await Department.create({
-                                    client: req.user._id,
-                                    place: place._id,
-                                    roles: [ROLES.PLACE_ROLES.BOSS_ROLE]
-                                });
-                                res.status(201).json(place);
-                            } catch (e) {
-                                e.status = 400;
-                                return next(e);
-                            }
-                        }
-                    });
+                    try {
+                        place = await place.supersave();
+                        await Department.create({
+                            client: req.user._id,
+                            place: place._id,
+                            roles: [ROLES.PLACE_ROLES.BOSS_ROLE]
+                        });
+                        res.status(201).json(place);
+                    } catch (e) {
+                        e.status = 400;
+                        return next(e);
+                    }
                 }
             }
         } catch (e) {
@@ -106,34 +87,13 @@ module.exports = {
             } else {
                 let place = await Place.findById(placeId);
                 if (place) {
-                    upload(req, res, async function (err) {
-                        if (err) {
-                            e.status = 400;
-                            return next(err);
-                        } else {
-                            if (!req.body.images)
-                                req.body.images = place.images;
-                            for (let field in req.files) {
-                                for (let fileKey in req.files[field]) {
-                                    if (req.files[field][fileKey].fieldname === "avatar") {
-                                        let image = "/upload/place/" + req.files[field][fileKey].filename;
-                                        req.body.avatar = image;
-                                    }
-                                    if (req.files[field][fileKey].fieldname === "images") {
-                                        let image = "/upload/place/" + req.files[field][fileKey].filename;
-                                        req.body.images.push(image);
-                                    }
-                                }
-                            }
-                            try {
-                                let updated = await place.superupdate(req.body);
-                                res.status(201).json(updated);
-                            } catch (e) {
-                                e.status = 400;
-                                return next(e);
-                            }
-                        }
-                    });
+                    try {
+                        let updated = await place.superupdate(req.body);
+                        res.status(201).json(updated);
+                    } catch (e) {
+                        e.status = 400;
+                        return next(e);
+                    }
                 } else {
                     let e = new Error();
                     e.message = "Not found";
@@ -144,6 +104,46 @@ module.exports = {
         } catch (e) {
             return next(e);
         }
+    },
+    upload(req, res, next) {
+        upload(req, res, async function (err) {
+            if (err) {
+                e.status = 400;
+                return next(err);
+            } else {
+                if (!req.body.images)
+                    req.body.images = [];
+                for (let field in req.files) {
+                    for (let fileKey in req.files[field]) {
+                        if (req.files[field][fileKey].fieldname === "avatar") {
+                            let image = "/upload/place/" + req.files[field][fileKey].filename;
+                            req.body.avatar = image;
+                        }
+                        if (req.files[field][fileKey].fieldname === "images") {
+                            let image = "/upload/place/" + req.files[field][fileKey].filename;
+                            req.body.images.push(image);
+                        }
+                    }
+                }
+                try {
+                    let query = {};
+                    let place = await Place.findById(req.params.id);
+                    if (req.body.avatar) {
+                        let toDelete = path.join(__dirname, "../public", place.avatar);
+                        fileHelper.deleteFiles(toDelete);
+                        query.avatar = req.body.avatar;
+                    }
+                    if (req.body.images) {
+                        query.$push = {images: req.body.images};
+                    }
+                    let placeRes = await Place.findByIdAndUpdate(req.params.id, query, {new: true});
+                    res.status(201).json(placeRes);
+                } catch (e) {
+                    e.status = 400;
+                    return next(e);
+                }
+            }
+        });
     },
     async removePlace(req, res, next) {
         let placeId = req.params.id;
--- routes/place.js
+++ routes/place.js
@@ -1,28 +1,32 @@
-const PlaceController = require('../controllers/PlaceController');
-let permission = require('../middleware/authorizarion/index');
-let ROLES = require('../config/roles');
-let PlaceRule = require('../middleware/authorizarion/rules/Place');
-let GlobalRule = require('../middleware/authorizarion/rules/Global');
-let Place = require('../models/Place');
-const express = require('express');
+const PlaceController = require("../controllers/PlaceController");
+let permission = require("../middleware/authorizarion/index");
+let ROLES = require("../config/roles");
+let PlaceRule = require("../middleware/authorizarion/rules/Place");
+let GlobalRule = require("../middleware/authorizarion/rules/Global");
+let Place = require("../models/Place");
+const express = require("express");
 
 const router = express.Router();
 
-router.route('/')
+router.route("/")
     .get(PlaceController.getPlaces)
     .post(
-        permission.rule(GlobalRule.updatable(Place.notUpdatable()),ROLES.GLOBAL_ROLES.ADMIN_ROLE),
+        permission.rule(GlobalRule.updatable(Place.notUpdatable()), ROLES.GLOBAL_ROLES.ADMIN_ROLE),
         PlaceController.createPlace
     );
-router.route('/:id')
+router.route("/:id")
     .get(PlaceController.getPlaceById)
     .put(
-        permission.rule(GlobalRule.updatable(Place.notUpdatable()),ROLES.GLOBAL_ROLES.ADMIN_ROLE),
-        permission.rule(PlaceRule.updatePlace,ROLES.GLOBAL_ROLES.ADMIN_ROLE),
+        permission.rule(GlobalRule.updatable(Place.notUpdatable()), ROLES.GLOBAL_ROLES.ADMIN_ROLE),
+        permission.rule(PlaceRule.updatePlace, ROLES.GLOBAL_ROLES.ADMIN_ROLE),
         PlaceController.updatePlace
     )
     .delete(
-        permission.rule(PlaceRule.deletePlace,ROLES.GLOBAL_ROLES.ADMIN_ROLE),
+        permission.rule(PlaceRule.deletePlace, ROLES.GLOBAL_ROLES.ADMIN_ROLE),
         PlaceController.removePlace
     );
+router.put("/:id/upload",
+    permission.rule(PlaceRule.updatePlace, ROLES.GLOBAL_ROLES.ADMIN_ROLE),
+    PlaceController.upload
+);
 module.exports = router;
